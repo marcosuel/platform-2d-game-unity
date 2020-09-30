fedaf0e1d6f3dd0cc2fd7bb88e7e7134c0aeed52
Marcos Suel <marcosuelns@alu.ufc.br>
2020-09-30 15:38:31 -0300

    added death system

--- Assets/Scripts/Managers/GameManagerScript.cs
+++ Assets/Scripts/Managers/GameManagerScript.cs
@@ -6,16 +6,30 @@ public class GameManagerScript : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject phaseGoal;
+    private GameObject phaseGoal = null;
+
+    private SceneManagerScript sceneManager;
 
     [SerializeField]
     private int requiredPoints = 100;
-    
+
+    private void Start()
+    {
+        sceneManager = GameObject.Find("Managers/SceneManager").GetComponent<SceneManagerScript>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(PlayerStatus.Pontos >= requiredPoints){
             phaseGoal.SetActive(true);
         }
+        if(PlayerStatus.Vidas < 0){
+            sceneManager.RestartGameScene();
+        }
+    }
+
+    public void RestartScene(){
+        sceneManager.RestartCurrentScene();
     }
 }
