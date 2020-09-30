f02178edfbc2e998626e0085a7aa460a2bcb547c
Marcos Suel <marcosuelns@alu.ufc.br>
2020-09-30 14:43:59 -0300

    added GameManager prefab (make PhaseGoal active when points is greater than a certain quantity)

---
+++ Assets/Scripts/Managers/GameManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject phaseGoal;
+
+    [SerializeField]
+    private int requiredPoints = 100;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerStatus.Pontos >= requiredPoints){
+            phaseGoal.SetActive(true);
+        }
+    }
+}
