261d40e4e58a44fed5d7549eca856c332e0a32af
Marcos Suel <marcosuelns@alu.ufc.br>
2020-10-01 15:53:16 -0300

    stealth bush done

--- Assets/Scripts/Enemies/DamageScript.cs
+++ Assets/Scripts/Enemies/DamageScript.cs
@@ -8,7 +8,10 @@ public class DamageScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player"){
-            other.gameObject.GetComponent<PlayerScript>().takeDamage();
+            PlayerScript script = other.gameObject.GetComponent<PlayerScript>();
+            if(!script.states.isHiding){
+                script.takeDamage();
+            }
         }
     }
 
--- Assets/Scripts/Enemies/EnemyScript.cs
+++ Assets/Scripts/Enemies/EnemyScript.cs
@@ -12,13 +12,21 @@ public class EnemyScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        var rb2d = other.gameObject.GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2(0f, 0f);
-        rb2d.AddForce(new Vector2(0f, 8f), ForceMode2D.Impulse);
-        health--;
-        if(health <= 0){
-            Destroy(this.transform.parent.gameObject);
-            Instantiate(collected, transform.position, transform.rotation);
+        if(other.gameObject.tag == "Player"){
+            PlayerScript script = other.gameObject.GetComponent<PlayerScript>();
+            if(script.states.isHiding){
+                return;
+            }
+
+
+            var rb2d = other.gameObject.GetComponent<Rigidbody2D>();
+            rb2d.velocity = new Vector2(0f, 0f);
+            rb2d.AddForce(new Vector2(0f, 8f), ForceMode2D.Impulse);
+            health--;
+            if(health <= 0){
+                Destroy(this.transform.parent.gameObject);
+                Instantiate(collected, transform.position, transform.rotation);
+            }
         }
     }
 }
