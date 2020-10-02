e7450f17aec9dbee4f0e99a1e13fb5a3b66f8632
Marcos Suel <marcosuelns@alu.ufc.br>
2020-10-02 15:28:34 -0300

    partially complete arrow and activator trap

---
+++ Assets/Scripts/Traps/ArrowScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public Vector2 dir;
+    
+    [SerializeField]
+    private float moveSpeed = 10f;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb2d.velocity = dir * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(this.gameObject, 2);
+    }
+
+    public void setDir(Vector2 direction){
+        this.dir = Vector2.up;
+        Debug.Log("tudo ok");
+    }
+}
---
+++ Assets/Scripts/Traps/ThrowerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowerScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform spawnPoint = null;
+
+    [SerializeField]
+    private GameObject arrow = null;
+
+    [SerializeField]
+    private Vector2 dir = Vector2.zero;
+
+    public void Throw(){
+        GameObject arrowObj = Instantiate(arrow, spawnPoint.position, transform.rotation);
+        arrow.GetComponent<ArrowScript>().dir = this.dir;
+    }
+}
