1f81a192b8ec61b5d2d71af885a2ec840505fbf2
Marcos Suel <marcosuelns@alu.ufc.br>
2020-10-03 15:35:48 -0300

    countdown done and scope in progress

---
+++ Assets/Scripts/Enemies/HunterScopeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterScopeScript : MonoBehaviour
+{
+    [SerializeField]
+    private float xSpeed = 0f;
+    
+    [SerializeField]
+    private float ySpeed = 1.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.Find("Player")){
+            GameObject plr = GameObject.Find("Player");
+            float xPos = Mathf.Lerp(transform.position.x, plr.transform.position.x, xSpeed * Time.deltaTime);
+            float yPos = Mathf.Lerp(transform.position.y, plr.transform.position.y, ySpeed * Time.deltaTime);
+            transform.position = new Vector2(xPos, yPos);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player"){
+            Debug.Log("POW");
+        }
+    }
+}
---
+++ Assets/Scripts/Misc/CountdownTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private float currentTime = 0f;
+    
+    [SerializeField]
+    private float startTime = 99f;
+
+    private Text countdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownText = transform.GetComponent<Text>();
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        if(currentTime <= 9.5 && currentTime > 0){
+            countdownText.color = Color.red;
+        }
+
+        if(currentTime <= 0){
+            currentTime = 0f;
+            countdownText.color = Color.black;
+        }
+        countdownText.text = currentTime.ToString("0");
+    }
+    
+}
