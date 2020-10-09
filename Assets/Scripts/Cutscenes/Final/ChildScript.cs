a45b879011068ccc3df05475ded4ccf18ef7b2f5
Marcos Suel <marcosuelns@alu.ufc.br>
2020-10-09 13:15:16 -0300

    Add cutscene on endgame

---
+++ Assets/Scripts/Cutscenes/Final/ChildScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildScript : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField]
+    private GameObject endText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void StartAnimation(){
+        anim.SetTrigger("tree");
+    }
+
+    public void StartRunAnimation(){
+        anim.SetTrigger("run");
+    }
+
+    public void StartIdleAnimation(){
+        anim.SetTrigger("idle");
+    }
+
+    public void StartEndText(){
+        Invoke("Activate", 0.5f);
+    }
+
+    private void Activate(){
+        endText.SetActive(true);
+    }
+}
